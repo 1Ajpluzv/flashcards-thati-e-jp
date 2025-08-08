2eb663bdc3a3753d06e28b81b1f2220341af375b
joão luz <joao.villela.costa@escola.pr.gov.br>
2025-08-08 12:25:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Futebol',   
+    'Qual o time verde e branco do Paraná?', 
+    'Coritiba Football CLub')
+
+criaCartao(
+    'Youtuber',  
+    'Qual o Maior Canal do YouTube no Brasil',  
+    'KondZilla' )
+
+criaCartao(
+    'Famosos',  
+    'Qual o nome da filha do meio de Vírginia Fonseca?',  
+    'Maria Flor Fonseca Serrão Rocha Costa' 
+)
+
+criaCartao(
+    'Cinema',   
+    'Qual o filme que ganhou o prêmio OSCAR em 2024', 
+    '"Oppenheimer", dirigido por Christopher Nolan' )
